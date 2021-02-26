af65ca0e21f68be4e712402e94f410aacdd98c4b
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-25 18:23:28 -0800

    fixed () error in stage app complied

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -4,8 +4,7 @@ import Cell from './Cell'
 const Stage = ({ stage }) => {
     return (
         <div>
-            {stage.map(row => row.map((cell, x) =>
-            <Cell key={x} type={cell[0]} />))}
+            {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
         </div>
     )
 }
