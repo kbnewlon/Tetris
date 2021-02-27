ecf77a6e0e73d02f937e5dc2a00ad7df16954dfd
Kayla Newlon <kbnewlon@hotmail.com>
2021-02-26 18:33:31 -0800

    keeps tetros from colliding into eachother

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -27,7 +27,14 @@ export const usePlayer = () => {
       const pos = clonedPlayer.pos.x;
       let offset = 1;
       while(checkCollision(clonedPlayer, stage, {x: 0, y: 0})){
-        
+        // checks collision of other tetros
+        clonedPlayer.pos.x += offset;
+        offset= -(offset + (offset > 0 ? 1: -1))
+        if(offset > clonedPlayer.tetromino[0].length){
+          rotate(clonedPlayer.tetromino, -dir)
+          clonedPlayer.pos.x = pos;
+          return;
+        }
       }
 
       setPlayer(clonedPlayer)
